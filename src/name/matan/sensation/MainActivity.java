03fc048d6cbd5b250b258a534e1935900a51602d
Adam Matan <adam@matan.name>
2012-09-08 00:04:32 +0300

    Initial commit

---
+++ src/name/matan/sensation/MainActivity.java
@@ -0,0 +1,84 @@
+package name.matan.sensation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+/**
+ * Registration form with link to agreement page.
+ * @author adamatan
+ *
+ */
+public class MainActivity extends Activity {
+
+	private NumberPicker np;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		np = (NumberPicker) findViewById(R.id.np);
+		np.setMaxValue(120);
+		np.setMinValue(0);
+		np.setValue(30);
+
+		Button tosButton = (Button) findViewById(R.id.button1);
+		tosButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//assertInput();
+			}
+		});
+	}
+	
+	private void isInputOK() {
+		
+		Toast.makeText(getApplicationContext(), "Is the phone number correct?", Toast.LENGTH_SHORT).show();
+		
+	}
+
+	/**
+	 * Shows an alert dialog before viewing the TOS
+	 * Credit: http://www.androidhive.info/2011/09/how-to-show-alert-dialog-in-android/
+	 */
+	public void onClick(View v) {
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+		alertDialog.setTitle(getString(R.string.tos_alert_title));
+		alertDialog.setMessage(getString(R.string.tos_alert_text));
+		alertDialog.setIcon(R.drawable.bananas_small);
+		// Yes - open the TOS
+		alertDialog.setPositiveButton(getString(R.string.tos_alert_button_yes), new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int which) {
+				//
+				Intent intent = new Intent(MainActivity.this, TosActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		// No - Go back to MainActivity
+		alertDialog.setNegativeButton(getString(R.string.tos_alert_button_no), new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(getApplicationContext(), getString(R.string.tos_alert_no_toast), 
+						Toast.LENGTH_SHORT).show();
+				dialog.cancel();
+			}
+		});
+		alertDialog.show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+}
---
+++ src/name/matan/sensation/TosActivity.java
@@ -0,0 +1,42 @@
+package name.matan.sensation;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.support.v4.app.NavUtils;
+
+/**
+ * Agreement page. Contents is downloaded from the web upon initialization.
+ * @author adamatan
+ */
+public class TosActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tos);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        WebView wv = (WebView) findViewById(R.id.tosWebView);
+        wv.loadUrl(getString(R.string.tos_activity_url));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_tos, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
